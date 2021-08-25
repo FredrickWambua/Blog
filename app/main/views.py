01d8c05260ab44d51b8b99619eea7637671e5fb8
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 16:52:58 +0300

    creates save picture method

--- app/main/views.py
+++ app/main/views.py
@@ -17,3 +17,9 @@ def index():
     page = request.args.get('page',1, type = int )
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + f_ext
+    picture_path = os.path.join('app/static/photos', picture_filename)
