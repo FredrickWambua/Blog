7eb83e22fca3814e532535d8dfe1afa20eab78d3
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 15:42:12 +0300

    creates the registration form authentification

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -10,3 +10,10 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password',validators=[Required()])
     remember = BooleanField('Remember Me!')
     submit = SubmitField('Login')
+
+class Registration_Form(FlaskForm):
+    username = StringField('Enter Your Username', validators=[Required(), Length(min=4, max=20)])
+    email = StringField('Email Address', validators=[Required(),Email()])
+    password = PasswordField('Password',validators = [Required(), EqualTo('password_confirm',message = 'Passwords must match')])
+    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    submit = SubmitField('Sign Up')
