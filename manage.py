b8ea66ffe7f500b0817257394fa20ee529cd79e8
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 22:59:21 +0300

    adds manager shell

--- manage.py
+++ manage.py
@@ -1,12 +1,19 @@
+import os
+from app.models import User
 from flask_script import Manager,Server
-from app import create_app
-from flask_migrate import Migrate, migrate
+from app import create_app,db
+from flask_migrate import Migrate
 
 
-app = create_app()
+app = create_app('development')
 
 manager = Manager(app)
+migrate = Migrate(app,db)
+manager.add_command('server', Server)
 
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
