54a7b7cdb012258230b5e1eb272f2e103be3c69a
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 23:07:10 +0300

    imports Python Images Library

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ import os
 from flask import render_template,redirect,url_for,abort,request,flash
 from app.models import User, Blog, Comment, Subscriber
 from .forms import UpdateProfile, CreateBlog
-from .. import main
+from .import main
 from app.requests import get_quotes
 from flask_login import login_required,current_user
 from ..email import mail_message
