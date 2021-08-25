0cf7f1be1ebbca1c03f9699e8189bbba929833a5
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 17:58:52 +0300

    creates view func for blog comments

--- app/main/views.py
+++ app/main/views.py
@@ -106,3 +106,12 @@ def blog(id):
     comments = Comment.query.filter_by(blog_id=id).all()
     blog = Blog.query.get(id)
     return render_template('blog.html',blog=blog,comments=comments)
+
+@main.route('/comment/<blog_id>', methods = ['Post','GET'])
+@login_required
+def comment(blog_id):
+    blog = Blog.query.get(blog_id)
+    comment =request.form.get('newcomment')
+    new_comment = Comment(comment = comment, user_id = current_user._get_current_object().id, blog_id=blog_id)
+    new_comment.save()
+    return redirect(url_for('main.blog',id = blog.id))
