c1b0b6300e9f02fbb839cb57dbbd33751dd779af
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-25 15:30:40 +0300

    creates login form class

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -4,3 +4,9 @@ from wtforms.validators import Required,Email,EqualTo, Length
 from wtforms import ValidationError
 from ..models import User
 
+
+class LoginForm(FlaskForm):
+    username = StringField('Username',validators=[Required()])
+    password = PasswordField('Password',validators=[Required()])
+    remember = BooleanField('Remember Me!')
+    submit = SubmitField('Login')
