0765149861fe899caf2d1a4c9edd6214aa85a187
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-26 09:45:20 +0300

    adds gunicorn

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from app import create_app,db
 from flask_migrate import Migrate
 
 
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
