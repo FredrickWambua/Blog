ce5256a07873070341b78b4b5c7b3077960b3827
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-08-26 14:05:24 +0300

    change db url

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with general configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://moringa:12345@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     
 
 class DevConfig(Config):
